e13fcc065976862db642fa34e1fe99d2f569422f
Yago Riveiro <yago.riveiro@gmail.com>
2022-01-05 21:52:49 +0000

    fix: gendoc

--- lua/dap-go/env.lua
+++ lua/dap-go/env.lua
@@ -1,30 +1,40 @@
-local M = {}
+---@brief [[
+--- This class wraps the `vim.fn.environ` with some functionalities and
+--- validations.
+---@brief ]]
+---@tag dap-go.env
+
+local _env = vim.fn.environ()
 
 ---@class Environment
-M._env = vim.fn.environ()
+local env = {}
+env._env = _env
+
+local mt = {}
+function mt:__index(k)
+  return env._env[k]
+end
+
+function mt:__newindex(k, v)
+  env._env[k] = v
+end
+
 
 --- Extends the current enviroment with new data.
----@param t table Table with new enviroment variables.
-function M.extend(t)
+---@param t table: extended enviroment variables to merge with current ones.
+function env.extend(t)
   assert(type(t) == 'table', 'input must be a table')
   for k, v in pairs(t) do
-    M._env[k] = v
+    env._env[k] = v
   end
 end
 
 --- Dumps the content of the class.
----@return Environment
-function M.dump()
-  return M._env
+---@return table: Table with all the enviroment
+function env.dump()
+  return env._env
 end
 
-local mt = {}
-function mt:__index(k)
-  return M._env[k]
-end
+setmetatable(env, mt)
 
-function mt:__newindex(k, v)
-  M._env[k] = v
-end
-
-return setmetatable(M, mt)
+return env
